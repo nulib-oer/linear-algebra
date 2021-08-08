9ba982d6e27f1a2232ef4252da41b14e6e1c5eed
Aaron Greicius <greicius@gmail.com>
2021-08-08 18:12:26 -0500

    Commutative diagram

--- build.sh
+++ build.sh
@@ -6,3 +6,17 @@ xsltproc -xinclude mathbook/xsl/pretext-html.xsl ptx/index.ptx
 
 mv *.html public
 mv knowl public
+
+# Below is my best guess at what the new build script should look like
+
+# mkdir -p public
+#
+# mkdir images
+#
+# mathbook/pretext/pretext -c latex-image -f all -d images ptx/index.ptx
+#
+# xsltproc -xinclude mathbook/xsl/pretext-html.xsl ptx/index.ptx
+#
+# mv *.html public
+# mv knowl public
+# mv images public
