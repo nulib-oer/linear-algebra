00e9753520345fa68df65f8b70f5ab81f4baca19
Aaron Greicius <greicius@gmail.com>
2021-08-12 16:34:28 -0500

    Messing with build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ xsltproc -xinclude mathbook/xsl/pretext-html.xsl ptx/index.ptx
 
 mv *.html public
 mv knowl public
+cp images public/images
 
 # Below is my best guess at what the new build script should look like
 
